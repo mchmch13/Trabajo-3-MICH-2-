6c12a9d79080ef3b625badd3aa79b47125419003
USUARIO <michellebarmot121198@gmail.com>
2023-02-23 13:09:03 -0600

    Agregar archivos de proyecto.

---
+++ Trabajo 3 MICH/Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim x As Integer
+        x = 1
+        Do While x <= 500
+            Console.Write(x)
+            Console.Write(" - ")
+            x = x + 1
+        Loop
+        Console.ReadKey()
+
+    End Sub
+
+End Module
